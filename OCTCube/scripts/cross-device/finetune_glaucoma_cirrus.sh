befcb7485cfd763af263b71cba290f20c93cc5f5
teddy <teddytw1209@gmail.com>
2025-06-21 17:47:20 -0400

    Update finetune_glaucoma_cirrus.sh

--- OCTCube/scripts/cross-device/finetune_glaucoma_cirrus.sh
+++ OCTCube/scripts/cross-device/finetune_glaucoma_cirrus.sh
@@ -6,8 +6,9 @@ ROOT=/blue/ruogu.fang
 prefix=tienyuchang
 LOG_DIR=$ROOT/log_pt/
 OUTPUT_DIR=./outputs_ft_st/finetune_glaucoma_3D_fewshot_10folds_correct_visit/
-CUDA_VISIBLE_DEVICES=1 python main_finetune_downstream_glaucoma_correct_visit.py --nb_classes 2 \
+python main_finetune_downstream_glaucoma_correct_visit.py --nb_classes 2 \
     --data_path $ROOT/$prefix/OCTCubeM/assets/ext_oph_datasets/glaucoma_processed/ \
+    --rank 0 \
     --dataset_mode volume \
     --iterate_mode visit \
     --name_split_char - \
