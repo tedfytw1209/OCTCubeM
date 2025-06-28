8f6b70d6824163c8e44e5417dcd9ae808cd44006
teddy <teddytw1209@gmail.com>
2025-06-27 22:05:18 -0400

    IRB2024 update

--- OCTCube/scripts/UFcohort_multirun.sh
+++ OCTCube/scripts/UFcohort_multirun.sh
@@ -7,7 +7,7 @@ Eval_score=${4:-"AUC"}
 ADDCMD=${5:-""}
 
 #bash scripts/UFcohort_multirun.sh scripts/finetune_UFcohort.sh flash_attn_vit_large_patch16 2 mcc --testval
-DATASETS=(AMD_all_split Cataract_all_split DR_all_split Glaucoma_all_split DR_all_split_binary Glaucoma_all_split_binary)  # List of datasets
+DATASETS=(AMD_all_split Cataract_all_split DR_all_split Glaucoma_all_split DR_binary_all_split Glaucoma_binary_all_split)  # List of datasets
 CLASSES=(2 2 6 6 2 2)  # Number of classes for each dataset
 TASK_MODES=(binary_cls binary_cls multi_cls multi_cls binary_cls binary_cls)  # Task mode, can be changed as needed
 for i in "${!DATASETS[@]}"
