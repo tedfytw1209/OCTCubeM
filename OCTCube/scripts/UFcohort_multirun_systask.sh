a6254d4ffa382cb15ea70eb09f2097a3247c568d
teddy <teddytw1209@gmail.com>
2025-10-11 23:12:43 -0400

    update

--- OCTCube/scripts/UFcohort_multirun_systask.sh
+++ OCTCube/scripts/UFcohort_multirun_systask.sh
@@ -1,4 +1,19 @@
-#! /bin/bash
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem-per-cpu=8gb
+#SBATCH --partition=hpg-turin
+#SBATCH --gpus=1
+#SBATCH --time=48:00:00
+#SBATCH --output=%x.%j.out
+#SBATCH --account=ruogu.fang
+#SBATCH --qos=ruogu.fang
+
+date;hostname;pwd
+
+module load conda
+conda activate octcube
 
 SCRIPT=$1
 MODEL=${2:-"flash_attn_vit_large_patch16"}
@@ -18,7 +33,7 @@ do
     DATASET="${DATASETS[$i]}"
     NUM_CLASS="${CLASSES[$i]}"
     TASK_MODE="${TASK_MODES[$i]}"
-    echo "sbatch $SCRIPT $DATASET $MODEL $NUM_CLASS $Eval_score $TASK_MODE $SUBSETNUM $ADDCMD"
+    echo "bash $SCRIPT $DATASET $MODEL $NUM_CLASS $Eval_score $TASK_MODE $SUBSETNUM $ADDCMD"
     # Submit the job to Slurm
-    sbatch $SCRIPT $DATASET $MODEL $NUM_CLASS $Eval_score $TASK_MODE $SUBSETNUM $ADDCMD
+    bash $SCRIPT $DATASET $MODEL $NUM_CLASS $Eval_score $TASK_MODE $SUBSETNUM $ADDCMD
 done
