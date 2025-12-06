adbff94d2e0e03dc2baebc49cac48f0d05927e16
teddy <teddytw1209@gmail.com>
2025-12-06 11:06:08 -0500

    multiclass rerun

--- OCTCube/scripts/UFcohort_multirun.sh
+++ OCTCube/scripts/UFcohort_multirun.sh
@@ -27,9 +27,9 @@ ADDCMD=${6:-""}
 #sbatch scripts/UFcohort_multirun.sh scripts/finetune_UFcohort_IRB2024v5.sh flash_attn_vit_large_patch16 2 AUC 500
 #DATASETS=(AMD_all_split Cataract_all_split DR_all_split Glaucoma_all_split DR_binary_all_split Glaucoma_binary_all_split DR_filtered_all_split DR_fbinary_all_split Glaucoma_filtered_all_split Glaucoma_fbinary_all_split)  # List of datasets
 #CLASSES=(2 2 6 6 2 2 5 2 4 2)  # Number of classes for each dataset
-DATASETS=(DR_fbinary_all_split Glaucoma_fbinary_all_split)  # List of datasets
-CLASSES=(2 2)  # Number of classes for each dataset
-TASK_MODES=(binary_cls binary_cls)  # Task mode, can be changed as needed
+DATASETS=(DR_filtered_all_split Glaucoma_filtered_all_split)  # List of datasets
+CLASSES=(5 4)  # Number of classes for each dataset
+TASK_MODES=(multi_cls multi_cls)  # Task mode, can be changed as needed
 for i in "${!DATASETS[@]}"
 do
     # Create a job name based on the variables
