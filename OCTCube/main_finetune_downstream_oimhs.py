6abf307f6508e0151ff6bdbf0dc0ead6021689a9
teddy <teddytw1209@gmail.com>
2026-02-02 10:49:50 -0500

    rank args

--- OCTCube/main_finetune_downstream_oimhs.py
+++ OCTCube/main_finetune_downstream_oimhs.py
@@ -249,6 +249,7 @@ def get_args_parser():
     parser.add_argument('--dist_on_itp', action='store_true')
     parser.add_argument('--dist_url', default='env://',
                         help='url used to set up distributed training')
+    parser.add_argument('--rank', default=-1, type=int)
 
     # wandb parameters
     parser.add_argument('--use_wandb', default=True, action='store_true',
