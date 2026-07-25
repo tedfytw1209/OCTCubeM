e3d6b2cafe4c342ad2570fc51de0c5ae9dee18a9
teddy <teddytw1209@gmail.com>
2026-07-25 11:51:17 +0800

    Update UFcohort_multirun_bootstrap_task_OCTCubeIR.sh

--- OCTCube/scripts/UFcohort_multirun_bootstrap_task_OCTCubeIR.sh
+++ OCTCube/scripts/UFcohort_multirun_bootstrap_task_OCTCubeIR.sh
@@ -29,8 +29,8 @@ ADDCMD=${6:-""}
 #DATASETS=(AMD_all_split Cataract_all_split DR_all_split Glaucoma_all_split DR_binary_all_split Glaucoma_binary_all_split DR_filtered_all_split DR_fbinary_all_split Glaucoma_filtered_all_split Glaucoma_fbinary_all_split)  # List of datasets
 #CLASSES=(2 2 6 6 2 2 5 2 4 2)  # Number of classes for each dataset
 #TASK_MODES=(binary_cls binary_cls multi_cls multi_cls binary_cls binary_cls multi_cls binary_cls multi_cls binary_cls)  # Task mode, can be changed as needed
-DATASETS=(Cataract_all_split DR_all_split Glaucoma_all_split DR_binary_all_split Glaucoma_binary_all_split)  # List of datasets
-CLASSES=(2 6 6 2 2)  # Number of classes for each dataset
+DATASETS=(AMD_all_split Cataract_all_split DR_all_split Glaucoma_all_split DR_binary_all_split Glaucoma_binary_all_split)  # List of datasets
+CLASSES=(2 2 6 6 2 2)  # Number of classes for each dataset
 TASK_MODES=(binary_cls multi_cls multi_cls binary_cls binary_cls)  # Task mode, can be changed as needed
 for i in "${!DATASETS[@]}"
 do
