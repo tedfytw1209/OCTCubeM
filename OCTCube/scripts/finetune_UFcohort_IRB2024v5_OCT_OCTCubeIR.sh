d0908cbcf80e583b8f6cc6762670421713832bf8
teddy <teddytw1209@gmail.com>
2026-09-18 19:55:46 -0400

    debug

--- OCTCube/scripts/finetune_UFcohort_IRB2024v5_OCT_OCTCubeIR.sh
+++ OCTCube/scripts/finetune_UFcohort_IRB2024v5_OCT_OCTCubeIR.sh
@@ -15,6 +15,10 @@ date;hostname;pwd
 module load conda
 conda activate octcube
 
+# Force Conda C++ runtime before importing PyTorch/SciPy extensions
+export LD_LIBRARY_PATH="$CONDA_PREFIX/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+export LD_PRELOAD="$CONDA_PREFIX/lib/libstdc++.so.6${LD_PRELOAD:+:$LD_PRELOAD}"
+
 STUDY=$1 #AMD_all_split 2, Cataract_all_split 2, DR_all_split 6, Glaucoma_all_split 6, DR_all_split_binary 2, Glaucoma_all_split_binary 2
 MODEL=${2:-"flash_attn_vit_large_patch16"}
 Num_CLASS=${3:-"2"}
